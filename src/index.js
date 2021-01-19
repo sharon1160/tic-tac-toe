496c645466e2c307f2caf7cb1fa145340bdb7715
Sharon Rossely CAlisson hullunquía Rosas <schullunquia@unsa.edu.pe>
2021-01-18 22:16:42 -0500

    Add a constructor to the Square and modifying render

--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import './index.css';
 
 class Square extends React.Component {
 
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" 
-              onClick={() => this.setState({value: 'X'})}
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -31,8 +25,19 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return (
+      <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
